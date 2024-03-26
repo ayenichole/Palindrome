93e99d4d1652ae5a67af3538ae4360773fc1923c
ayenichole <nicholermdenga@gmail.com>
2024-03-26 16:42:27 +0100

    commit main

--- src/Palindrom.java
+++ src/Palindrom.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Palindrom {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Palindrom {
+    public static void main(String[] args) throws IOException {
+
+        String palindrom;
+        boolean check = true;
+        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter your word please");
+        palindrom= br.readLine().toLowerCase();
+
+        for(int i=0; i< palindrom.length()/2; i++ ){
+         if(palindrom.charAt(i) != palindrom.charAt(palindrom.length() - i -1)) {
+          System.out.println("It is not a palindrome");
+          check = false;
+          break;
+
+            }
+        }
+        if(check) {
+            System.out.println(palindrom +  "is a palindrome");
+        } else {
+            System.out.println(palindrom + "It is not a palindrome");
+        }
+
+
+
+
+
+    }
 }
